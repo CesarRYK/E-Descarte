07e9072b17a28cc8810e65d37ee6d044c49b34be
unknown <h.leonardocsantos@gmail.com>
2023-08-08 18:58:20 -0400

    Finalização das Tarefas.js

--- src/screens/Tarefas.js
+++ src/screens/Tarefas.js
@@ -1,4 +1,5 @@
 import { StyleSheet, View, Pressable, Text, ImageBackground } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 export default Tarefas;
 
@@ -10,7 +11,30 @@ function Tarefas({}) {
                 source={require('./imagens/Fundo.png')}
                 resizeMode="cover"
             />
-            
+            <View>
+                <Text style={styles.texto1}>Realize um descarte de lixo eletrônico quinzenalmente.</Text>
+            </View>
+
+            <View>
+                <Text style={styles.texto2}>Indique um amigo para usar o aplicativo.</Text>
+            </View>
+
+            <View>
+                <Text style={styles.texto3}>Conclua 2 tarefas para receber um bônus.</Text>
+            </View>
+
+            <View>
+                <Text style={styles.texto4}>0/3</Text>
+                <FontAwesome5 style={styles.Icone}
+                name='tasks'
+                size={80}
+                color='#fff'
+                
+                />
+
+            </View>
+
+
         </View>
         
     );
@@ -31,5 +55,54 @@ const styles = StyleSheet.create({
         height: 800,
         resizeMode: 'cover',
         justifyContent: 'center',
+    },
+
+    texto1: {
+        top: -280,
+        left: 0,
+        color: '#5f9ea0',
+        borderWidth: 0,
+        borderColor: '#000000',
+        backgroundColor: '#fff',
+        padding: 15,
+        borderRadius: 40,
+        marginBottom: 20,
+    },
+
+    texto2: {
+        top: -250,
+        left: 0,
+        color: '#5f9ea0',
+        borderWidth: 0,
+        borderColor: '#000000',
+        backgroundColor: '#fff',
+        padding: 20,
+        borderRadius: 40,
+        marginBottom: 20,
+    },
+
+    texto3: {
+        top: -200,
+        left: 0,
+        color: '#5f9ea0',
+        borderWidth: 0,
+        borderColor: '#000000',
+        backgroundColor: '#fff',
+        padding: 20,
+        borderRadius: 40,
+        marginBottom: 20,
+    },
+
+    texto4: {
+        top: -120,
+        left: 10,
+        fontSize: 35,
+        color: '#f0f8ff',
+    },
+
+    Icone: {
+        top: -120,
+        left: 0,
+
     }
 });
