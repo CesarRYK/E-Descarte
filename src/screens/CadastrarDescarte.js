d9b88a8a37b0c7fede2fcb6f69bf9abbd2540410
unknown <h.leonardocsantos@gmail.com>
2023-09-13 19:22:33 -0400

    Tela CadastrarDescarte

--- src/screens/CadastrarDescarte.js
+++ src/screens/CadastrarDescarte.js
@@ -19,6 +19,20 @@ export default function CadastrarDescarte({  }) {
                 />
                 </Pressable>
             
+                <Text style={styles.TextoTitulo}>E-DESCARTE</Text>
+                <Text style={styles.Text1}>DESCARTES</Text>
+
+
+                <Text style={styles.Adicione}>1- Adicione uma imagem ao seu Descarte:</Text>
+                <Text style={styles.Preencha}>2- Preencha os campos abaixo:</Text>
+
+                <Text style={styles.Concluir}>CONCLUIR</Text>
+                <Text style={styles.TirarFoto}>TIRAR FOTO</Text>
+                <Text style={styles.Importar}> IMPORTAR</Text>
+
+
+
+
             </View>
     )
 }
@@ -30,8 +44,71 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between', 
     },
 
+    TextoTitulo: {
+        color: '#f0fff0',
+        fontSize: 28,
+        top: -920,      
+    },
+
+    Text1: {
+        color: '#7fffd4',
+        top: -920,
+    },
+
     LogOut: {
-        top: -1080,
+        top: -880,
         left: -155,
+    },
+
+    Concluir: {
+        color: '#5f9ea0',
+        top: -320,
+        borderWidth: 3,
+        borderRadius: 40,
+        backgroundColor: '#f8f8ff',
+        borderColor: '#f5fffa',
+        padding: 8,
+        width: 150,
+        height: 50,
+        fontSize: 20,
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+
+
+    TirarFoto: {
+        color: '#5f9ea0',
+        top: -490,
+        left: 40,
+        borderWidth: 3,
+        borderRadius: 40,
+        backgroundColor: '#f8f8ff',
+        borderColor: '#f5fffa',
+        padding: 8,
+        width: 120,
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+
+    Importar: {
+        color: '#5f9ea0',
+        top: -490,
+        left: 40,
+        borderWidth: 3,
+        borderRadius: 40,
+        backgroundColor: '#f8f8ff',
+        borderColor: '#f5fffa',
+        padding: 8,
+        width: 120,
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+
+    Adicione: {
+
+    },
+
+    Preencha: {
+
     },
 });
