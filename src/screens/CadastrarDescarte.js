92ab8cd4e23f3a6cd72ead9223f1f49c50381204
unknown <h.leonardocsantos@gmail.com>
2023-09-18 21:34:51 -0400

    inicio da lista de unidade da tela CadastrarDescarte

--- src/screens/CadastrarDescarte.js
+++ src/screens/CadastrarDescarte.js
@@ -1,16 +1,41 @@
 import {View, Text, StyleSheet, Image, Pressable} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import * as React from 'react';
+import { List } from 'react-native-paper';
 
 
 export default function CadastrarDescarte({ navigation }) {
+    const [expanded, setExpanded] = React.useState(true);
+    const handlePress = () => setExpanded(!expanded);
+
     return(
             <View style={styles.container}>
+        
                 <LinearGradient style={{
                 height: 950, 
                 width: 600, 
                 borderRadius: 5}}
 	            colors={['#009582','#009452']}>
-                </LinearGradient>
+              </LinearGradient>
+                
+              //<List.Section
+                style={styles.ClSele} title="">
+      <List.Accordion
+        title="Uncontrolled Accordion"
+        left={props => <List.Icon {...props} icon="folder" />}>
+        <List.Item title="First item" />
+        <List.Item title="Second item" />
+      </List.Accordion>
+
+      <List.Accordion
+        title="Controlled Accordion"
+        left={props => <List.Icon {...props} icon="folder" />}
+        expanded={expanded}
+        onPress={handlePress}>
+        <List.Item title="First item" />
+        <List.Item title="Second item" />
+      </List.Accordion>
+    </List.Section>//
 
                 <Image
                 style={styles.imagem}
